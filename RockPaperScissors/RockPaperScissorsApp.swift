dcebfe8e05657b7b6696c615bfea81300af656c6
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-08-20 15:58:15 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Suhail Khan on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
